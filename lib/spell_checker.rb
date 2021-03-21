17e3736365bbb31bb234f5528efa7c4eac630d8e
calavell <calavell@tcd.ie>
2021-03-21 10:28:34 +0000

    Will add tildas to incorrect words

--- lib/spell_checker.rb
+++ lib/spell_checker.rb
@@ -1,13 +1,15 @@
 def spell_checker(input_string, dictionary)
-    spell_checked_string = input_string.split(' ')
-    spell_checked_string.each do |word|
-        correct_word(word, dictionary)
+    input_string_as_array = input_string.split(' ')
+    input_string_as_array.map! do |word|
+        check_word(word, dictionary)
     end
-    spell_checked_string.join(' ')
+    input_string_as_array.join(' ')
 end
 
-def correct_word(word, dictionary)
-    unless dictionary.include? word
-        word = '~' + word + '~'
+def check_word(word, dictionary)
+    if dictionary.include? word
+        word
+    else
+        '~' + word + '~'
     end
 end
--- spec/spell_checker_spec.rb
+++ spec/spell_checker_spec.rb
@@ -4,4 +4,8 @@ describe '#spell_checker' do
     it 'returns the given string, if it is spelled correctly' do
         expect(spell_checker('test string', ['test', 'string'])). to eq 'test string'
     end
+
+    it 'returns a string with ~ around each incorrect word' do
+        expect(spell_checker('tett string', ['test', 'string'])). to eq '~tett~ string'
+    end
 end
