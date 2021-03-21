336e9ad81d4cfaf51ee3aeb98bfa5eaa0e051e4c
calavell <calavell@tcd.ie>
2021-03-21 10:38:24 +0000

    Raises TypeError for non string inputs

--- lib/spell_checker.rb
+++ lib/spell_checker.rb
@@ -1,4 +1,6 @@
 def spell_checker(input_string, dictionary)
+    raise TypeError.new 'expected a string input' unless input_string.kind_of? String
+
     input_string_as_array = input_string.split(' ')
     input_string_as_array.map! do |word|
         check_word(word, dictionary)
@@ -10,6 +12,6 @@ def check_word(word, dictionary)
     if dictionary.include? word
         word
     else
-        '~' + word + '~'
+        "~#{word}~"
     end
 end
--- spec/spell_checker_spec.rb
+++ spec/spell_checker_spec.rb
@@ -8,4 +8,8 @@ describe '#spell_checker' do
     it 'returns a string with ~ around each incorrect word' do
         expect(spell_checker('tett string', ['test', 'string'])). to eq '~tett~ string'
     end
+
+    it 'raises type error if input string argument is not a string' do
+        expect { spell_checker(6, ['test', 'dictionary']) }.to raise_error(TypeError)
+    end
 end
